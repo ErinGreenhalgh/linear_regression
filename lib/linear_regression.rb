3e36a8b42e7ca814422ffe085a2796afab4887f9
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-06-01 08:55:10 -0600

    setting up the learning class

---
+++ lib/linear_regression.rb
@@ -0,0 +1,26 @@
+require 'hypothesis_line'
+
+# given a test data set
+# and a learning rate
+# and a random theta0 and theta1
+#1. check how bad we did (cost function)
+#2. then keep updating the theta1 and theta0 until cost levels out
+# also want to set some kind of parameters for it to stop
+
+class Learner
+
+  def initialize(learning_rate, test_data)
+    @learning_rate = learning_rate
+    @test_data = test_data
+  end
+
+  def random_theta0
+
+  end
+
+  def random_theta1
+
+  end
+
+
+end
